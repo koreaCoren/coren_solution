fea079392037338d7efc2bc9712be49c1c2b4cac
pelshad <akuaru@gmail.com>
2022-10-20 09:57:55 +0900

    프론트 구조 설계중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,93 +1,25 @@
-import { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Index from './components/Layout/Index';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import UserRouter from './routers/UserRouter';
 
-//MVC 테스트용
 
 const App = () => {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-
-  //insert
-  const insert = async () => {
-    const param = {
-      'id' : {id}.id,
-      'pw' : {pw}.pw
-    }
-    console.log(param);
-    try {
-      const result = await axios.post("MVC/backend/user/ins_user", param);
-      setId('');
-      setPw('');
-      console.log(result);
-    } catch {
-      console.error("통신 실패");
-    }
-  };
-
-  //select
-  const select = async () => {
-    try {
-      const result = await axios.get("MVC/backend/user/sel_user", );
-      const data = result.data.result;
-      console.log(data);
-    } catch {
-      console.error("통신 실패");
-    }
-  };
-  
-  //update
-  const update = async () => {
-    const param = {
-      'id' : {id}.id,
-      'cid' : {pw}.pw
-    }
-    try {
-      //console.log(`${{id}.id} ${{pw}.pw}`);
-      if(!{id}.id == '' && !{pw}.pw == ''){
-        const result = await axios.post("MVC/backend/user/upd_user", param);
-        console.log(result);
-        setId('');
-        setPw('');
-      } else {
-        alert("두 값을 모두 입력하세요.");
-      }
-    } catch {
-      console.error("통신 실패");
-    }
-  }
-
-  //delete
-  const deldata = async () => {
-    try {
-      const param = {
-        'id' : {id}.id
-      }
-      console.log(param);
-      const result = await axios.put("MVC/backend/user/del_user", param);
-      console.log(result);
-    } catch {
-      console.error("통신 실패");
-    }
-  };
-  
-
-  return (
-    <div>
-      <input type="text" value={id} onChange={(e) => {
-        e.preventDefault();
-        setId(e.target.value);
-      }}></input>
-
-      <input type="password" value={pw} onChange={(e) => {
-        e.preventDefault();
-        setPw(e.target.value);
-      }}></input>
-      <div><button onClick={insert}>insert</button></div> 
-      <div><button onClick={select}>select</button></div>
-      <div><button onClick={update}>update</button></div>
-      <div><button onClick={deldata}>delete</button></div>
-    </div>
-  );
-}
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Index />}></Route>
+                    <Route path='/User/*' element={<UserRouter/>}></Route>
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
