1309d7457ad324641188b989ad4212a4e6268ecf
zyakal <pkd1426@gmail.com>
2022-10-21 16:52:06 +0900

    1021

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,32 +1,34 @@
 /* eslint-disable no-fallthrough */
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "asset/css/login/login.css";
-import axios from 'axios';
+import axios from "axios";
 
 const Login = () => {
-
     const [getId, setId] = useState();
     const [getPassword, setPassword] = useState();
 
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        const url = "http://192.168.0.86/MVC/backend/user/userLogin";
+        const url = "http://192.168.0.86/MVC/backend/user/sel_user";
         const loginData = {
             id: getId,
             pw: getPassword,
-        }
+        };
 
-        await axios.post(url, loginData).then((res) => {
-            console.log(res);
-            console.log("성공");
-        }).catch((error) => {
-            console.log(error);
-            console.log("응 안됨 돌아가");
-        })
-    }
+        await axios
+            .post(url, loginData)
+            .then((res) => {
+                console.log(res);
+                console.log("성공");
+            })
+            .catch((error) => {
+                console.log(error);
+                console.log("응 안됨 돌아가");
+            });
+    };
 
     const onChange = (e) => {
         const value = e.target.value;
@@ -39,7 +41,7 @@ const Login = () => {
             default:
                 break;
         }
-    }
+    };
 
     return (
         <>
@@ -47,16 +49,30 @@ const Login = () => {
                 <h2>로그인</h2>
 
                 <form onSubmit={onSubmit}>
-                    <input type="text" onChange={onChange} name="id" placeholder='아이디' />
-                    <input type="password" onChange={onChange} name="pw" placeholder='비밀번호' />
+                    <input
+                        type="text"
+                        onChange={onChange}
+                        name="id"
+                        placeholder="아이디"
+                    />
+                    <input
+                        type="password"
+                        onChange={onChange}
+                        name="pw"
+                        placeholder="비밀번호"
+                    />
 
-                    <button type='submit'>로그인</button>
-                    <Link to="/login/register" className='modify'>회원가입</Link>
-                    <Link to="/recover" className='recover'>아이디 / 비밀번호 찾기</Link>
+                    <button type="submit">로그인</button>
+                    <Link to="/login/register" className="modify">
+                        회원가입
+                    </Link>
+                    <Link to="/recover" className="recover">
+                        아이디 / 비밀번호 찾기
+                    </Link>
                 </form>
             </div>
         </>
     );
 };
 
-export default Login; 
+export default Login;
