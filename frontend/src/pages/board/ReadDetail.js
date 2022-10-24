05b7dd030016d24be2f1b9120995face62a83b20
zyakal <pkd1426@gmail.com>
2022-10-24 10:27:51 +0900

    10.24

--- frontend/src/pages/board/ReadDetail.js
+++ frontend/src/pages/board/ReadDetail.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const ReadDateil = (props) => {
     const { id } = useParams();
     return (
         <>
             <p>제목 : {props.getBoardList[id]?.title}</p>
-            <p>게시판내용 : {props.getBoardList[id]?.content}</p>
+            <p>게시판내용 : {props.getBoardList[id]?.ctnt}</p>
         </>
     );
 };
--- frontend/src/routers/Board.js
+++ frontend/src/routers/Board.js
@@ -1,31 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
-import "asset/css/board/read.css"
+import "asset/css/board/read.css";
 
-import Read from 'pages/board/Read';
-import ReadDetail from 'pages/board/ReadDetail';
+import Read from "pages/board/Read";
+import ReadDetail from "pages/board/ReadDetail";
 
 const Board = () => {
     const [getBoardList, setBoardList] = useState([]);
     const getBoard = async () => {
         const url = `${process.env.REACT_APP_API_URL}/Board/sel_board`;
-        await axios.get(url).then((res) => {
-            // setBoardList(res.data);
-            console.log(res.data);
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
+        await axios
+            .get(url)
+            .then((res) => {
+                setBoardList(res.data);
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
     useEffect(() => {
         getBoard();
-    }, [getBoardList])
+    }, [getBoardList]);
     return (
         <>
             <Routes>
-                <Route path='/*' element={<Read getBoardList={getBoardList} />}></Route>
-                <Route path='/board/detail/:id/*' element={<ReadDetail getBoardList={getBoardList} />}></Route>
+                <Route
+                    path="/*"
+                    element={<Read getBoardList={getBoardList} />}></Route>
+                <Route
+                    path="/board/detail/:id/*"
+                    element={
+                        <ReadDetail getBoardList={getBoardList} />
+                    }></Route>
             </Routes>
         </>
     );
