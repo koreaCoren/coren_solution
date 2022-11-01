5e6cef8bb714207cc3f49d6253a379e5195f8164
zyakal <pkd1426@gmail.com>
2022-11-01 10:51:40 +0900

    1101

--- backend/index.php
+++
@@ -1,7 +0,0 @@
-<?php    
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    require_once 'application/libs/Config.php';
-    require_once 'application/libs/Autoload.php';
-    new application\libs\Application();
