2ac9d00404e078c673f559bd225ed1b9459bcd7e
pelshad <akuaru@gmail.com>
2022-11-02 17:10:18 +0900

    토큰체크

--- backend/application/controllers/UserController.php
+++ backend/application/controllers/UserController.php
@@ -67,8 +67,8 @@ class UserController extends Controller {
     }
 
     //토큰 체크
-    public function check_token(){
+    public function checkToken(){
         $json = getJson();
-        return $this->model->check_token($json);
+        return $this->model->checkToken($json);
     }
 }
